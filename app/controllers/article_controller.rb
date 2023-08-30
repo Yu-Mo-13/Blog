a26a9a2ba89d74bc035480eb58866cf15ba39287
Yuya Motozono <motozono.tech@gmail.com>
2023-08-30 13:46:09 +0900

    2023/8/30作業分コミット

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,10 +1,22 @@
 class ArticleController < ApplicationController
   def index
+    @articles = Article.all.order('created_at DESC')
   end
 
   def detail
+    @article = Article.find_by(id: params[:id])
+  end
+
+  def new
+    @article = Article.new()
   end
 
   def post
+    @article = Article.new(content: params[:content], title: params[:title])
+    if @article.save
+      redirect_to("/")
+    else
+      render("article/new")
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'article/index'
-  get 'article/detail'
-  get 'article/post'
+  get '/' => 'article#index'
+  get 'article/new' => 'article#new'
+  get 'article/:id' => 'article#detail'
+  post 'article/post' => 'article#post'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
