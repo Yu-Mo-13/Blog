e7f317eb0c96b28d063e1fe9469bf1040d81e434
Yuya Motozono <motozono.tech@gmail.com>
2023-09-19 23:07:10 +0900

    9/19作業分コミット

--- app/controllers/snslinks_controller.rb
+++ app/controllers/snslinks_controller.rb
@@ -1,10 +1,26 @@
 class SnslinksController < ApplicationController
   def index
+    @snslink = Snslink.all.order('created_at DESC')
   end
 
   def edit
+    @snslink = Snslink.find_by(id: params[:id])
   end
 
   def post
+    @snslink = Snslink.new(app: params[:app], link: params[link])
+  end
+
+  def new
+    @snslink = Snslink.new()
+  end
+
+  def update
+    @snslink = Snslink.find_by(id: params[:id])
+    if @snslink.update(app: params[:app], link: params[link])
+      redirect_to("/snslinks/")
+    else
+      render("snslinks/edit")
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_03_043257) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_19_140048) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "keyword"
@@ -21,6 +21,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_03_043257) do
 
   create_table "profiles", force: :cascade do |t|
     t.string "content"
+    t.string "string"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "snslinks", force: :cascade do |t|
+    t.string "app"
+    t.string "link"
     t.string "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
