17f508ecb062ec1bb0e6bb6e9cab6c22560e2f55
Yuya Motozono <motozono.tech@gmail.com>
2023-09-24 19:10:23 +0900

    SNSリンクページ画面作成。追加画面、一覧画面、詳細画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'snslinks/new' => 'snslinks#new'
   get 'profiles/' => 'profiles#index'
   get 'profiles/edit' => 'profiles#edit'
   post 'profiles/post' => 'profiles#post'
   get 'snslinks/' => 'snslinks#index'
   get 'snslinks/edit' => 'snslinks#edit'
+  get 'snslinks/new' => 'snslinks#new'
   get 'snslinks/:id/detail' => 'snslinks#detail'
   post 'snslinks/post' => 'snslinks#post'
   get '/' => 'article#index'
